4516a31b936c45ec74fc9de87582e3e5494c6500
jenmwa <jenmwa@gmail.com>
2023-05-23 23:22:00 +0200

    upd.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-// import { RouterView } from 'vue-router';
-// import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -12,7 +12,7 @@
 
   <router-view></router-view>
 
-  <!-- <div>
+  <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -20,7 +20,7 @@
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" /> -->
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -33,7 +33,7 @@
 .v-leave-to {
   opacity: 0;
 }
-/* 
+
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -45,6 +45,6 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-} */
+}
 
 </style>
