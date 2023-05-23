60d03b19aec82a35a75e9a90872402cd2c8106b2
jenmwa <jenmwa@gmail.com>
2023-05-23 23:06:33 +0200

    vite.config upd.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [],
+  base: '/todo-with-vue/',
+  define: {
+    'process.env': {},
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/style/variables";',
+      },
+    },
+  },
+});
