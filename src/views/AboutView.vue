cbedebb239e3d8f975d63217e2ade7ddeb04856f
jenmwa <jenmwa@gmail.com>
2023-05-25 22:23:17 +0200

    small css changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from '../components/HelloWorld.vue';
+
+</script>
 
 <template>
     about this page View
+    <HelloWorld msg="Vite + Vue" />
 </template>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+console.log(import.meta.env.VITE_API_KEY)
+</script>
+
 <template>
     todos View
 </template>
